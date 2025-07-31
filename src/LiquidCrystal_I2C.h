75edde7b276314d5d7bbc26dc3d04b9c16a30aa2
Shin Taeha <mirinae3145@g.skku.edu>
2025-07-31 14:08:59 +0900

    constants standard

--- src/LiquidCrystal_I2C.h
+++ src/LiquidCrystal_I2C.h
@@ -1,5 +1,4 @@
-#ifndef FDB_LIQUID_CRYSTAL_I2C_H
-#define FDB_LIQUID_CRYSTAL_I2C_H
+#pragma once
 
 #include <Arduino.h>
 #include <inttypes.h>
@@ -8,57 +7,57 @@
 
 namespace LCD {
 	namespace Function {
-		inline constexpr uint8_t Bit8     = 0x10; // 8-bit interface
-		inline constexpr uint8_t Bit4     = 0x00; // 4-bit interface
-		inline constexpr uint8_t Lines1   = 0x00; // 1-line display
-		inline constexpr uint8_t Lines2   = 0x08; // 2-line display
-		inline constexpr uint8_t Font5x8  = 0x00; // 5×8 dot font
-		inline constexpr uint8_t Font5x10 = 0x04; // 5×10 dot font
+		constexpr byte Bit8     = 0x10; // 8-bit interface
+		constexpr byte Bit4     = 0x00; // 4-bit interface
+		constexpr byte Lines1   = 0x00; // 1-line display
+		constexpr byte Lines2   = 0x08; // 2-line display
+		constexpr byte Font5x8  = 0x00; // 5×8 dot font
+		constexpr byte Font5x10 = 0x04; // 5×10 dot font
 	}
 
 	namespace Control {
-		inline constexpr uint8_t On        = 0x04; // display on
-		inline constexpr uint8_t Off       = 0x00; // display off
-		inline constexpr uint8_t CursorOn  = 0x02; // cursor on
-		inline constexpr uint8_t CursorOff = 0x00; // cursor off
-		inline constexpr uint8_t BlinkOn   = 0x01; // blink on
-		inline constexpr uint8_t BlinkOff  = 0x00; // blink off
+		constexpr byte On        = 0x04; // display on
+		constexpr byte Off       = 0x00; // display off
+		constexpr byte CursorOn  = 0x02; // cursor on
+		constexpr byte CursorOff = 0x00; // cursor off
+		constexpr byte BlinkOn   = 0x01; // blink on
+		constexpr byte BlinkOff  = 0x00; // blink off
 	}
 
 	namespace Mode {
-		inline constexpr uint8_t Left       = 0x02; // I/D = 1 (increment)
-		inline constexpr uint8_t Right      = 0x00; // I/D = 0 (decrement)
-		inline constexpr uint8_t ShiftIncr  = 0x01; // S = 1
-		inline constexpr uint8_t ShiftDecr  = 0x00; // S = 0
+		constexpr byte Left       = 0x02; // I/D = 1 (increment)
+		constexpr byte Right      = 0x00; // I/D = 0 (decrement)
+		constexpr byte ShiftIncr  = 0x01; // S = 1
+		constexpr byte ShiftDecr  = 0x00; // S = 0
 	}
 
 	namespace Shift {
-		inline constexpr uint8_t DisplayMove = 0x08; // SC = 1
-		inline constexpr uint8_t CursorMove  = 0x00; // SC = 0
-		inline constexpr uint8_t MoveRight   = 0x04; // RL = 1
-		inline constexpr uint8_t MoveLeft    = 0x00; // RL = 0
+		constexpr byte DisplayMove = 0x08; // SC = 1
+		constexpr byte CursorMove  = 0x00; // SC = 0
+		constexpr byte MoveRight   = 0x04; // RL = 1
+		constexpr byte MoveLeft    = 0x00; // RL = 0
 	}
 
 	namespace Backlight {
-		inline constexpr uint8_t On  = 0x08;
-		inline constexpr uint8_t Off = 0x00;
+		constexpr byte On  = 0x08;
+		constexpr byte Off = 0x00;
 	}
 
 	namespace Pin {
-		inline constexpr uint8_t En = 0x04; // Enable
-		inline constexpr uint8_t Rw = 0x02; // Read/Write
-		inline constexpr uint8_t Rs = 0x01; // Register Select
+		constexpr byte En = 0x04; // Enable
+		constexpr byte Rw = 0x02; // Read/Write
+		constexpr byte Rs = 0x01; // Register Select
 	}
 
 	namespace Command {
-		inline constexpr uint8_t ClearDisplay   = 0x01;
-		inline constexpr uint8_t ReturnHome     = 0x02;
-		inline constexpr uint8_t EntryModeSet   = 0x04;
-		inline constexpr uint8_t DisplayControl = 0x08;
-		inline constexpr uint8_t CursorShift    = 0x10;
-		inline constexpr uint8_t FunctionSet    = 0x20;
-		inline constexpr uint8_t SetCGRAMAddr   = 0x40;
-		inline constexpr uint8_t SetDDRAMAddr   = 0x80;
+		constexpr byte ClearDisplay   = 0x01;
+		constexpr byte ReturnHome     = 0x02;
+		constexpr byte EntryModeSet   = 0x04;
+		constexpr byte DisplayControl = 0x08;
+		constexpr byte CursorShift    = 0x10;
+		constexpr byte FunctionSet    = 0x20;
+		constexpr byte SetCGRAMAddr   = 0x40;
+		constexpr byte SetDDRAMAddr   = 0x80;
 	}
 
 } // namespace LCD
@@ -172,5 +171,3 @@ private:
 	uint8_t _backlightval;
 	TwoWire& _Wire;
 };
-
-#endif // FDB_LIQUID_CRYSTAL_I2C_H
